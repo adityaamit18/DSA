5a3a10541cae175ccdd7679d1e65bfb6f3991f3a
Aditya Amit Trivedi <128981641+adityaamit18@users.noreply.github.com>
2024-07-02 20:07:16 +0530

    Create LC189 - 189. Rotate Array.cpp

---
+++ Arrays/LC189 - 189. Rotate Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%nums.size();
+        if(k==0) return;
+        int temp[k];
+        for (int i=0;i<k;i++) {
+            temp[i]=nums[n-k+i];
+        }
+        for (int i=n-1;i>=k;i--) {
+            nums[i] = nums[i - k];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+};
