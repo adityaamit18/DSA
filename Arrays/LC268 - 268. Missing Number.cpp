733d71a4794b73a459c5e480eecf5ebfd19ecee5
Aditya Amit Trivedi <128981641+adityaamit18@users.noreply.github.com>
2024-07-16 20:15:32 +0530

    Update LC268 - 268. Missing Number.cpp

--- Arrays/LC268 - 268. Missing Number.cpp
+++ Arrays/LC268 - 268. Missing Number.cpp
@@ -31,3 +31,81 @@ public:
     return (sum-s2);
     }
 };
+////////////////////
+#include<bits/stdc++.h>
+using namespace std;
+
+//Brute
+int missno1(int nums[],int n){
+
+    for(int i=0;i<=n;i++){
+            int flag=0;
+            for(int j=0;j<n-1;j++){
+                if(nums[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0) return i;
+        }
+        return -1;
+}
+
+//Better -using hashing
+int missno2(int nums[],int n){
+
+    int hash[n+1]={0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[nums[i]]++;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if(hash[i]==0) return i;
+    }  
+    return -1;
+}
+
+int missno3(int nums[],int n){
+    int sum=n*(n+1)/2;
+    int s2=0;
+    for (int i = 0; i < n; i++)
+    {
+        s2 += nums[i];
+    }
+    return (sum-s2);
+    
+}
+
+int missno4(int nums[],int n){
+    int xor1=0,xor2=0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        xor2= xor2 ^ nums[i];
+        xor1= xor1 ^ (i);
+    }
+    xor1 = xor1 ^ n;
+    return xor1 ^ xor2;
+    
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>nums[i];
+    }
+    
+    //cout<<missno1(nums , n);
+    //cout<<missno2(nums , n);
+    // cout<<missno3(nums , n);
+    cout<<missno4(nums , n);
+
+    
+    return 0;
+}
