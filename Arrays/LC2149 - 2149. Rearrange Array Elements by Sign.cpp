37b95a6f7c2ac06f84be0a777aecb539d16d4acb
Aditya Amit Trivedi <128981641+adityaamit18@users.noreply.github.com>
2024-07-21 12:05:10 +0530

    Create LC2149 - 2149. Rearrange Array Elements by Sign.cpp

---
+++ Arrays/LC2149 - 2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size()); 
+
+        int negIndex = 1; 
+        int posIndex = 0;  
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 0) {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+};
