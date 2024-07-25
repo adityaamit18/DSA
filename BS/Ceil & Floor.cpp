be713de755b9bab3a0b459e5f0d4672c39e35913
Aditya Amit Trivedi <128981641+adityaamit18@users.noreply.github.com>
2024-07-25 22:03:37 +0530

    Update Ceil & Floor.cpp

--- BS/Ceil & Floor.cpp
+++ BS/Ceil & Floor.cpp
@@ -73,6 +73,30 @@ int main() {
         Solution ob;
         auto ans = ob.getFloorAndCeil(x, arr);
         cout << ans[0] << " " << ans[1] << "\n";
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+auto ceilIt = lower_bound(a.begin(), a.end(), x);
+    int ceiling = (ceilIt == a.end()) ? -1 : *ceilIt;
+
+    // Finding the floor
+    auto floorIt = upper_bound(a.begin(), a.end(), x);
+    if (floorIt == a.begin()) {
+        // No element less than or equal to x
+        return {-1, ceiling};
+    } else {
+        --floorIt;
+    }
+    int floor = *floorIt;
+
+    // Adjusting the ceiling and floor values
+    if (ceiling < x) ceiling = -1;
+    if (floor > x) floor = -1;
+
+    return {floor, ceiling};}*/
     }
     return 0;
 }
